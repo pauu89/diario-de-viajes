923bb0eeb5b0910a2db9e4b5bf50fdcbfce920bc
pauu89 <paula.rodriguez89@live.com.ar>
2018-03-22 13:55:49 +0400

    galeria
    galeria agregada a la pagina de fotografia

--- onclick.js
+++ onclick.js
@@ -3,3 +3,17 @@ $( document ).ready(function() {
      $(this).css("color", "red");
  });
 })
+
+$( document ).ready(function() {
+	
+$("img#logo").animate({
+    width: "70%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+}, 1500 )
+
+ });
+
+
